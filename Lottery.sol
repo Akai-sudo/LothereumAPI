fea344b6776a19c3543ed0904ee0a73833fa9c2e
Akai-sudo <68597421+Akai-sudo@users.noreply.github.com>
2021-12-08 15:01:31 +0100

    Update Lottery.sol

--- Lottery.sol
+++ Lottery.sol
@@ -1,122 +1,57 @@
-pragma solidity ^0.4.25;
-//import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
-import "github.com/provable-things/ethereum-api/provableAPI_0.4.25.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
 
-contract Lottery is usingProvable {
-    string public betNumber;
-    string public result;
-    address public house;
-    address public better;
-    enum State { Created, Betted, Paidout, Inactive }
-    State public state;
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
-    uint randNonce = 0;
+/**
+ * THIS IS AN EXAMPLE CONTRACT WHICH USES HARDCODED VALUES FOR CLARITY.
+ * PLEASE DO NOT USE THIS CODE IN PRODUCTION.
+ */
+
+/**
+ * Request testnet LINK and ETH here: https://faucets.chain.link/
+ * Find information on LINK Token Contracts and get the latest ETH and LINK faucets here: https://docs.chain.link/docs/link-token-contracts/
+ */
+ 
+contract RandomNumberConsumer is VRFConsumerBase {
     
-    event newProvableQuery(string description);
-    event Aborted();
-    event Betted();
-    event Released();
-	
-    constructor() public {
-        house = msg.sender;
-        state = State.Created;
-    }
-
-    modifier condition(bool _condition) {
-        require(_condition);
-        _;
-    }
-
-    modifier onlyBetter() {
-        require(msg.sender == better);
-        _;
-    }
-
-    modifier onlyHouse() {
-        require(msg.sender == house);
-        _;
-    }
-
-    modifier inState(State _state) {
-        require(state == _state);
-        _;
-    }
-
-	//source: https://ethereum.stackexchange.com/questions/30912/how-to-compare-strings-in-solidity
-    function compareStrings (string a, string b) public pure returns (bool){
-       return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
-	}
-	
-    function bet(string _betNumber)
-        public
-        inState(State.Created)
-        condition(msg.value == (1 ether))
-        payable
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    
+    uint256 public randomResult;
+    
+    /**
+     * Constructor inherits VRFConsumerBase
+     * 
+     * Network: Kovan
+     * Chainlink VRF Coordinator address: 0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9
+     * LINK token address:                0xa36085F69e2889c224210F603D836748e7dC0088
+     * Key Hash: 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4
+     */
+    constructor() 
+        VRFConsumerBase(
+            0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator
+            0xa36085F69e2889c224210F603D836748e7dC0088  // LINK Token
+        )
     {
-        emit Betted();
-        better = msg.sender;
-        betNumber = _betNumber;
-        state = State.Betted;
+        keyHash = 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4;
+        fee = 0.1 * 10 ** 18; // 0.1 LINK (Varies by network)
     }
     
-    function abort()
-        public
-        onlyHouse
-        inState(State.Created)
-    {
-        emit Aborted();
-        state = State.Inactive;
-        house.transfer(address(this).balance);
-    }
-    
-    function release() 
-        payable 
-        public
-        onlyHouse
-        inState(State.Betted)
-    {
-        if (provable_getPrice("URL") > address(this).balance) {
-            emit newProvableQuery("Oraclize query was NOT sent, please add some ETH to cover for the query fee");
-        } else {
-            emit newProvableQuery("Oraclize query was sent, standing by for the answer..");
-            provable_query("URL", "json(https://jacksonng.org/codetest/random.php/).random");
-        }
+    /** 
+     * Requests randomness 
+     */
+    function getRandomNumber() public returns (bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
+        return requestRandomness(keyHash, fee);
     }
 
-     function rand()
-        public
-        view
-    returns(uint256)
-    {
-        uint256 seed = uint256(keccak256(abi.encodePacked(
-            block.timestamp + block.difficulty +
-            ((uint256(keccak256(abi.encodePacked(block.coinbase)))) / (now)) +
-            block.gaslimit + 
-            ((uint256(keccak256(abi.encodePacked(msg.sender)))) / (now)) +
-            block.number
-        )));
-
-        return (seed - ((seed / 1000) * 1000));
+    /**
+     * Callback function used by VRF Coordinator
+     */
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        randomResult = randomness;
     }
 
-    function randGen(uint _modulus) internal returns(uint)
-    {
-        randNonce++;
-        return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
-    }
-	
-	function __callback(bytes32 myid, string res) public {
-        if (msg.sender != provable_cbAddress()) revert(); //bil je throw pa je deprecated
-        result = res; 
-        
-        if (compareStrings(result,betNumber)) {
-            better.transfer(address(this).balance);            
-        }
-        else {
-            house.transfer(address(this).balance);               
-        }
-
-        emit Released();
-        state = State.Paidout;
-    }
+    // function withdrawLink() external {} - Implement a withdraw function to avoid locking your LINK in the contract
 }
